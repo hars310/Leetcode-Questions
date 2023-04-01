33b36a384ccfab2d796f67af4bee1024b26b0265
har-sh310 <111885288+har-sh310@users.noreply.github.com>
2023-04-01 09:58:17 +0530

    Time: 7 ms (18.53%), Space: 8.9 MB (47.60%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+        int removeElement(vector<int> &nums, int val)
+        {
+            int size = nums.size();
+
+            int j = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[j++] = nums[i];
+                }
+            }
+            return j;
+        }
+};
